6396614b0db48097051fa77af2dbc7aaad7318fd
Mustiskooo <110821966+Mustiskooo@users.noreply.github.com>
2026-07-02 18:07:31 +0300

    Refactor isAdmin function to use ADMINS set

--- app.js
+++ app.js
@@ -39,9 +39,14 @@ export function protectPage() {
     });
   });
 }
+/* adminler (#babalar sözünü tutar#) */
+const ADMINS = new Set([
+  "yazicimustafayazici05@gmail.com",
+  "HOCANINMAİLİ@gmail.com"
+]);
 
 export function isAdmin(user) {
-  return user?.email === "yazicimustafayazici05@gmail.com";
+  return user && ADMINS.has(user.email);
 }
 
 export async function logout() {
